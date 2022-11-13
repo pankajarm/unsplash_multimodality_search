74bda910bbdc1d6979cfdb315337daefd23da6ff
Pankaj Mathur <pankymathur@gmail.com>
2022-11-12 23:36:12 -0800

    dadD

--- frontend/ui.py
+++ frontend/ui.py
@@ -28,16 +28,15 @@ elif input_media == "image":
 if "matches" in locals():
     for match in matches[0]:
         # print("match:", match)
-        try:
-            print("match.uri", match.uri)
-            # matched_images.append(image)
-            # image_id = str(str(match.uri).split("/")[1]).split(".")[0]
-            # st.markdown(f"[![Src]({image})]({match.uri})")
-            image = resize_image(match.uri, resize_factor=IMAGE_MAX_SIZE)
-            st.image(image, use_column_width="auto")
-            st.markdown(f"Source({match.uri})")
-        except:
-            continue
+        print("match.uri", match.uri)
+        # just check here
+        # if "https://" in str(match.uri):
+        # matched_images.append(image)
+        # image_id = str(str(match.uri).split("/")[1]).split(".")[0]
+        # st.markdown(f"[![Src]({image})]({match.uri})")
+        image = resize_image(match.uri, resize_factor=IMAGE_MAX_SIZE)
+        st.image(image, use_column_width="auto")
+        st.markdown(f"Source({match.uri})")
         
 
     # image_iterator = paginator("Select a result page", matched_images,on_sidebar=False)
